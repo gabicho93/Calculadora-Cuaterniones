0eebc69a72945a3b1d7733174e10c88bbd1aca3e
gabicho93 <angabriel50@hotmail.com>
2016-11-23 15:14:46 -0500

    se ha añadido la clase cuaternion y algunos metodos

---
+++ Quaternioon/Quaternioon/Cuaternion.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Motor_fisica2
+{
+    public class Cuaternion
+    {
+        #region atributos
+        private float a;
+        private float x;
+        private float y;
+        private float z;
+        #endregion
+
+        #region constructores
+        public Cuaternion()
+        {
+        }
+        public Cuaternion(float A, float X, float Y, float Z)
+        {
+            a = A;
+            x = X;
+            y = Y;
+            z = Z;
+
+        }
+        public Cuaternion(Cuaternion q)
+            : this(q.a, q.x, q.y, q.z)
+        {
+
+        }
+        #endregion
+
+        #region propiedades
+        public float A
+        {
+            get
+            {
+                return a;
+            }
+        }
+        public float X
+        {
+            get
+            {
+                return x;
+            }
+        }
+
+        public float Y
+        {
+            get
+            {
+                return y;
+            }
+        }
+        public float Z
+        {
+            get
+            {
+                return z;
+            }
+        }
+        #endregion
+
+        #region propiedades_staticas
+        //vector 1
+        public static Cuaternion One
+        {
+            get
+            {
+                return new Cuaternion(0, 1, 1, 1);
+            }
+        }
+
+        public static Cuaternion Zero
+        {
+            get
+            {
+                return new Cuaternion(0, 0, 0, 0);
+            }
+        }
+        public static Cuaternion UnitX
+        {
+            get
+            {
+                return new Cuaternion(0, 1, 0, 0);
+            }
+        }
+
+        public static Cuaternion UnitY
+        {
+            get
+            {
+                return new Cuaternion(0, 0, 1, 0);
+            }
+        }
+
+        public static Cuaternion UnitZ
+        {
+            get
+            {
+                return new Cuaternion(0, 0, 0, 1);
+            }
+        }
+        #endregion
+
+        #region metodos
+
+        public static float modulo(Cuaternion Q)
+        {
+            float m;
+            m = Convert.ToSingle(Math.Sqrt(
+                Math.Pow(Q.A, 2) + Math.Pow(Q.X, 2) +
+                    Math.Pow(Q.Y, 2) +
+                    Math.Pow(Q.Z, 2)));
+            return m;
+
+        }
+
+        public float modulo()
+        {
+            return Convert.ToSingle(
+                (Math.Sqrt(Convert.ToDouble(a * a + x * x + y * y + z * z))));
+        }
+
+        public void norma()
+        {
+            float d = modulo();
+            if (d > 0)
+            {
+                a = a / d;
+                x = x / d;
+                y = y / d;
+                z = z / d;
+            }
+
+        }
+
+
+        #endregion
+
+        #region metodos_staticos
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Quaternioon/Quaternioon/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Motor_fisica2;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //rotar
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float theta; 
+            float p1,p2,p3,p4;
+            float d1, d2, d3, d4;
+            
+            theta = Convert.ToSingle(Convert.ToDouble(this.textBox1.Text));
+            
+            p1 = Convert.ToSingle(Convert.ToDouble(this.textBox2.Text));
+            p2 = Convert.ToSingle(Convert.ToDouble(this.textBox3.Text));
+            p3 = Convert.ToSingle(Convert.ToDouble(this.textBox4.Text));
+            p4 = Convert.ToSingle(Convert.ToDouble(this.textBox11.Text));
+            Cuaternion p= new Cuaternion(p1,p2,p3,p4);
+
+            d1 = Convert.ToSingle(Convert.ToDouble(this.textBox5.Text));
+            d2 = Convert.ToSingle(Convert.ToDouble(this.textBox6.Text));
+            d3 = Convert.ToSingle(Convert.ToDouble(this.textBox7.Text));
+            d4 = Convert.ToSingle(Convert.ToDouble(this.textBox12.Text));
+            Cuaternion d= new Cuaternion(d1,d2,d3,d4);
+                                    
+            Cuaternion q1 = new Cuaternion();
+            q1 = Cuaternion.normalizar(d);
+            Cuaternion q2 = new Cuaternion();
+            q2 = Cuaternion.transf_rotar(theta, q1);
+            Cuaternion q3 = new Cuaternion();
+            q3 = Cuaternion.conjugado(q2);
+            Cuaternion q4 = new Cuaternion();
+            q4 =Cuaternion.producto_quater(q2,p);
+            Cuaternion Qr = new Cuaternion();
+            Qr = Cuaternion.producto_quater(q4,q3);
+
+            this.textBox8.Text = Qr.A.ToString();
+            this.textBox9.Text = Qr.X.ToString();
+            this.textBox10.Text =Qr.Y.ToString();
+            this.textBox13.Text =Qr.Z.ToString();
+
+            
+                
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float p1, p2, p3, p4;
+            float d1, d2, d3, d4;
+
+            p1 = Convert.ToSingle(Convert.ToDouble(this.textBox2.Text));
+            p2 = Convert.ToSingle(Convert.ToDouble(this.textBox3.Text));
+            p3 = Convert.ToSingle(Convert.ToDouble(this.textBox4.Text));
+            p4 = Convert.ToSingle(Convert.ToDouble(this.textBox11.Text));
+            Cuaternion p = new Cuaternion(p1, p2, p3, p4);
+
+            d1 = Convert.ToSingle(Convert.ToDouble(this.textBox5.Text));
+            d2 = Convert.ToSingle(Convert.ToDouble(this.textBox6.Text));
+            d3 = Convert.ToSingle(Convert.ToDouble(this.textBox7.Text));
+            d4 = Convert.ToSingle(Convert.ToDouble(this.textBox12.Text));
+            Cuaternion d = new Cuaternion(d1, d2, d3, d4);
+
+            Cuaternion q4 = new Cuaternion();
+            q4 = Cuaternion.producto_quater(p, d);
+
+            this.textBox8.Text = q4.A.ToString();
+            this.textBox9.Text = q4.X.ToString();
+            this.textBox10.Text = q4.Y.ToString();
+            this.textBox13.Text = q4.Z.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float p1, p2, p3, p4;
+            p1 = Convert.ToSingle(Convert.ToDouble(this.textBox2.Text));
+            p2 = Convert.ToSingle(Convert.ToDouble(this.textBox3.Text));
+            p3 = Convert.ToSingle(Convert.ToDouble(this.textBox4.Text));
+            p4 = Convert.ToSingle(Convert.ToDouble(this.textBox11.Text));
+            Cuaternion p = new Cuaternion(p1, p2, p3, p4);
+
+            Cuaternion q4 = new Cuaternion();
+            q4 = Cuaternion.quater_inverso(p);
+
+            this.textBox8.Text = q4.A.ToString();
+            this.textBox9.Text = q4.X.ToString();
+            this.textBox10.Text = q4.Y.ToString();
+            this.textBox13.Text = q4.Z.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            float theta;
+            float p1, p2, p3, p4;
+
+            theta = Convert.ToSingle(Convert.ToDouble(this.textBox1.Text));
+
+            p1 = Convert.ToSingle(Convert.ToDouble(this.textBox5.Text));
+            p2 = Convert.ToSingle(Convert.ToDouble(this.textBox6.Text));
+            p3 = Convert.ToSingle(Convert.ToDouble(this.textBox7.Text));
+            p4 = Convert.ToSingle(Convert.ToDouble(this.textBox12.Text));
+            Cuaternion p = new Cuaternion(p1, p2, p3, p4);
+
+            Cuaternion q4 = new Cuaternion();
+            q4 = Cuaternion.transf_rotar(theta, p);
+
+            this.textBox8.Text = q4.A.ToString();
+            this.textBox9.Text = q4.X.ToString();
+            this.textBox10.Text = q4.Y.ToString();
+            this.textBox13.Text = q4.Z.ToString();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            float p1, p2, p3, p4;
+
+          
+            p1 = Convert.ToSingle(Convert.ToDouble(this.textBox2.Text));
+            p2 = Convert.ToSingle(Convert.ToDouble(this.textBox3.Text));
+            p3 = Convert.ToSingle(Convert.ToDouble(this.textBox4.Text));
+            p4 = Convert.ToSingle(Convert.ToDouble(this.textBox11.Text));
+            Cuaternion p = new Cuaternion(p1, p2, p3, p4);
+
+            Cuaternion q4 = new Cuaternion();
+            q4 = Cuaternion.conjugado( p);
+
+            this.textBox8.Text = q4.A.ToString();
+            this.textBox9.Text = q4.X.ToString();
+            this.textBox10.Text = q4.Y.ToString();
+            this.textBox13.Text = q4.Z.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            float p1, p2, p3, p4;
+
+
+            p1 = Convert.ToSingle(Convert.ToDouble(this.textBox2.Text));
+            p2 = Convert.ToSingle(Convert.ToDouble(this.textBox3.Text));
+            p3 = Convert.ToSingle(Convert.ToDouble(this.textBox4.Text));
+            p4 = Convert.ToSingle(Convert.ToDouble(this.textBox11.Text));
+            Cuaternion p = new Cuaternion(p1, p2, p3, p4);
+
+            Cuaternion q4 = new Cuaternion();
+            q4 = Cuaternion.normalizar(p);
+
+            this.textBox8.Text = q4.A.ToString();
+            this.textBox9.Text = q4.X.ToString();
+            this.textBox10.Text = q4.Y.ToString();
+            this.textBox13.Text = q4.Z.ToString();
+        }
+    }
+}
